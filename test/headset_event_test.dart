59d478d558c059fb7b93b42cf0298900de88e41f
Rafael Delos Santos <rkdelossantos@gmail.com>
2020-07-03 21:16:46 +1000

    Update pubspec and test

--- test/headset_event_test.dart
+++ test/headset_event_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:headset_event/headset_event.dart';
+import 'package:headset_connection_event/headset_event.dart';
 import 'package:mockito/mockito.dart';
 
 typedef DetectPluggedCallback = Function(HeadsetState payload);
@@ -11,22 +11,18 @@ void main() {
 
   setUp(() {
     methodChannel = MockMethodChannel();
-    he = HeadsetEvent.private(methodChannel);    
+    he = HeadsetEvent.private(methodChannel);
   });
 
   test('getCurrentState', () async {
-    when(methodChannel.invokeMethod<int>('getCurrentState'))
-        .thenAnswer((Invocation invoke) => Future<int>.value(0));
+    when(methodChannel.invokeMethod<int>('getCurrentState')).thenAnswer((Invocation invoke) => Future<int>.value(0));
     expect(await he.getCurrentState, HeadsetState.DISCONNECT);
 
-    when(methodChannel.invokeMethod<int>('getCurrentState'))
-        .thenAnswer((Invocation invoke) => Future<int>.value(1));
+    when(methodChannel.invokeMethod<int>('getCurrentState')).thenAnswer((Invocation invoke) => Future<int>.value(1));
     expect(await he.getCurrentState, HeadsetState.CONNECT);
 
-    when(methodChannel.invokeMethod<int>('getCurrentState'))
-        .thenAnswer((Invocation invoke) => Future<int>.value(-1));
+    when(methodChannel.invokeMethod<int>('getCurrentState')).thenAnswer((Invocation invoke) => Future<int>.value(-1));
     expect(await he.getCurrentState, HeadsetState.DISCONNECT);
-
   });
 }
 
